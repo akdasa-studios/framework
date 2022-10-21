1caf6c445398a7c28744a43ea4be216008ea379a
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-10-21 11:21:26 +0000

    feat: uuid identity test

--- tests/domain/models/uuid-identity.spec.ts
+++ tests/domain/models/uuid-identity.spec.ts
@@ -12,7 +12,14 @@ describe('UuidIdentity', () => {
       expect(identity.value).toBe('e81c6c1b-d80e-43b4-838c-0451f9027fdc')
     })
 
-    it('throws xception if incorrect uuid passed', () => {
+    it('generates new uuid if no value passes', () => {
+      const identity = new UuidIdentity()
+      expect(identity.value).not.toBeUndefined()
+      expect(identity.value).not.toEqual('00000000-0000-0000-0000-000000000000')
+
+    })
+
+    it('throws exception if incorrect uuid passed', () => {
       expect(() => new UuidIdentity('123')).toThrowError('123 is not valid UUID')
     })
   })
