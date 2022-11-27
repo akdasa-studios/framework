cec9bec1cfbcb35f380268e282b7ec83da55cd35
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-11-27 08:40:37 +0000

    build cjs only

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-rm -rf dist.cjs
-rm -rf dist.esm
-
-npx tsc --module commonjs --outdir dist.cjs
-npx tsc --module es6 --outdir dist.esm
-
-npx resolve-tspaths -o ./dist.cjs
-npx resolve-tspaths -o ./dist.esm
