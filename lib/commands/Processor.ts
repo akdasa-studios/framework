15d94d12963909f9948a57fff0de537168fb4695
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-16 12:17:26 +0000

    refactoring

--- lib/commands/Processor.ts
+++ lib/commands/Processor.ts
@@ -1,84 +1,9 @@
-import { AnyResult, Fail, NoResult, Ok, Result } from '@lib/core'
-import { Command, AnyCommand } from './Command'
+import { AnyResult, Fail, Ok, Result } from '@lib/core'
+import { Command } from './Command'
+import { ExecutionStack } from './ExecutionStack'
+import { ProcessorResult } from './ProcessorResult'
+import { Transaction } from './Transaction'
 
-
-export class ProcessorResult<TCommandResult extends AnyResult> {
-  constructor(
-    public readonly processorResult: Result<boolean, string>,
-    public readonly commandResult: TCommandResult = NoResult as TCommandResult,
-  ) {}
-
-  /**
-   * Indicates if the command is executed.
-   * @returns True if the command is executed, otherwise false.
-   * @note This property is true if the command is executed successfully or failed.
-   */
-  get isCommandExecuted(): boolean {
-    return this.processorResult.isSuccess
-  }
-
-  /**
-   * Indicates if the command is executed successfully.
-   * @returns True if the command is executed successfully, otherwise false.
-   */
-  get isCommandSucceeded(): boolean {
-    return this.commandResult.isSuccess
-  }
-
-  /**
-   * Gets the result of the executed command.
-   * @returns {TCommandResult['value']} Returns the result of the executed command.
-   */
-  get value() : TCommandResult['value'] | TCommandResult['error'] {
-    return this.commandResult.value
-  }
-}
-
-/**
- * Transaction is a marker for a group of commands.
- */
-export class Transaction {
-  /**
-   * Initialize a new instance of the Transaction class.
-   * @param value Unique identifier of the transaction.
-   */
-  constructor(public readonly value: string) {}
-}
-
-/**
- * ExecutionStack is a stack of executed commands.
- */
-interface ExecutionHistoryLine {
-  command: AnyCommand,
-  transactionId: Transaction | undefined
-}
-
-/**
- * ExecutionStack is a stack of executed commands.
- */
-class ExecutionStack {
-  private lines: ExecutionHistoryLine[] = []
-
-  public push(command: AnyCommand, transactionId?: Transaction) {
-    this.lines.push({command, transactionId})
-  }
-
-  public includes(command: AnyCommand) {
-    return this.lines.map(r => r.command).includes(command)
-  }
-
-  public pop(): readonly AnyCommand[] {
-    const lastRow = this.lines.pop()
-    if (!lastRow) { return [] }
-    if (!lastRow.transactionId) { return [lastRow.command] }
-
-    const result = [lastRow.command]
-    while (this.lines.at(-1)?.transactionId === lastRow.transactionId) {
-      result.push((this.lines.pop() as ExecutionHistoryLine).command)
-    }
-    return result
-  }
-}
 
 /**
  * Processor executes commands.
---
+++ lib/commands/ProcessorResult.ts
@@ -0,0 +1,35 @@
+import { AnyResult, NoResult, Result } from '@lib/core';
+
+
+
+export class ProcessorResult<TCommandResult extends AnyResult> {
+  constructor(
+    public readonly processorResult: Result<boolean, string>,
+    public readonly commandResult: TCommandResult = NoResult as TCommandResult
+  ) { }
+
+  /**
+   * Indicates if the command is executed.
+   * @returns True if the command is executed, otherwise false.
+   * @note This property is true if the command is executed successfully or failed.
+   */
+  get isCommandExecuted(): boolean {
+    return this.processorResult.isSuccess;
+  }
+
+  /**
+   * Indicates if the command is executed successfully.
+   * @returns True if the command is executed successfully, otherwise false.
+   */
+  get isCommandSucceeded(): boolean {
+    return this.commandResult.isSuccess;
+  }
+
+  /**
+   * Gets the result of the executed command.
+   * @returns {TCommandResult['value']} Returns the result of the executed command.
+   */
+  get value(): TCommandResult['value'] | TCommandResult['error'] {
+    return this.commandResult.value;
+  }
+}
