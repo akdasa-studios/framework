813894014bbbd8313849a7df5df29f13e7857f78
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2022-12-27 17:36:50 +0100

    async repos

--- lib/persistence/memory/InMemoryRepository.ts
+++ lib/persistence/memory/InMemoryRepository.ts
@@ -11,34 +11,35 @@ export class InMemoryRepository<
   protected entities = new Map<TEntity['id'], TEntity>()
   protected processor = new InMemoryQueryProcessor<TEntity>()
 
-  public all(): readonly TEntity[] {
-    return Array.from(this.entities.values())
+  async all(): Promise<Result<readonly TEntity[]>> {
+    return Result.ok(Array.from(this.entities.values()))
   }
 
-  public save(entity: TEntity): Result<void, string> {
+  async save(entity: TEntity): Promise<Result<void, string>> {
     const copy = Object.create(entity)
     Object.assign(copy, entity)
     this.entities.set(entity.id.value, copy)
     return Result.ok()
   }
 
-  public get(id: TEntity['id']): Result<TEntity, string> {
+  async get(id: TEntity['id']): Promise<Result<TEntity, string>> {
     const value = this.entities.get(id.value)
     if (!value) { return Result.fail(`Entity '${id.value}' not found`) }
     return Result.ok(value)
   }
 
-  public exists(id: TEntity['id']): boolean {
+  async exists(id: TEntity['id']): Promise<boolean> {
     return this.entities.has(id.value)
   }
 
-  public find(query: Query<TEntity>): readonly TEntity[] {
+  async find(query: Query<TEntity>): Promise<Result<readonly TEntity[]>> {
     const entities = Array.from(this.entities.values())
-    return this.processor.execute(query, entities)
+    return Result.ok(this.processor.execute(query, entities))
   }
 
-  public delete(id: TEntity['id']): Result<void, string> {
-    if (!this.exists(id)) {
+  async delete(id: TEntity['id']): Promise<Result<void, string>> {
+    const isExists = await this.exists(id)
+    if (!isExists) {
       return Result.fail(`Entity '${id.value}' not found`)
     }
     this.entities.delete(id.value)
--- tests/persistence/memory/InMemoryRepository.spec.ts
+++ tests/persistence/memory/InMemoryRepository.spec.ts
@@ -8,7 +8,7 @@ describe('InMemoryRepository', () => {
   let order2: Order
   let repository: InMemoryRepository<Order>
 
-  beforeEach(() => {
+  beforeEach(async () => {
     order1 = new Order(
       new OrderId('123'), 'John',
       new Address('2nd Avenue', 'New York', 'Zip'),
@@ -20,8 +20,8 @@ describe('InMemoryRepository', () => {
       200
     )
     repository = new InMemoryRepository<Order>()
-    repository.save(order1)
-    repository.save(order2)
+    await repository.save(order1)
+    await repository.save(order2)
   })
 
   /* -------------------------------------------------------------------------- */
@@ -29,19 +29,21 @@ describe('InMemoryRepository', () => {
   /* -------------------------------------------------------------------------- */
 
   describe('.save', () => {
-    it('saves entity', () => {
-      expect(() => repository.exists(order1.id)).toBeTruthy()
+    it('saves entity', async () => {
+      expect(await repository.exists(order1.id)).toBeTruthy()
     })
 
-    it('do not update entity until it saved', () => {
+    it('do not update entity until it saved', async () => {
       order1.clientName = 'George'
-      expect(repository.get(order1.id).value.clientName).toEqual('John')
+      const getResult = await repository.get(order1.id)
+      expect(getResult.value.clientName).toEqual('John')
     })
 
-    it('updates entity if it saved', () => {
+    it('updates entity if it saved', async () => {
       order1.clientName = 'George'
-      repository.save(order1)
-      expect(repository.get(order1.id).value.clientName).toEqual('George')
+      await repository.save(order1)
+      const getResult = await repository.get(order1.id)
+      expect(getResult.value.clientName).toEqual('George')
     })
   })
 
@@ -50,9 +52,9 @@ describe('InMemoryRepository', () => {
   /* -------------------------------------------------------------------------- */
 
   describe('.all', () => {
-    it('returns all entities', () => {
-      const result = repository.all()
-      expect(result).toEqual([order1, order2])
+    it('returns all entities', async () => {
+      const result = await repository.all()
+      expect(result.value).toEqual([order1, order2])
     })
   })
 
@@ -61,14 +63,14 @@ describe('InMemoryRepository', () => {
   /* -------------------------------------------------------------------------- */
 
   describe('.get', () => {
-    it('returns entity', () => {
-      const result = repository.get(order1.id).value
+    it('returns entity', async () => {
+      const result = (await repository.get(order1.id)).value
       expect(result.equals(order1)).toBeTruthy()
     })
 
-    it('returns error if entity does not exist', () => {
+    it('returns error if entity does not exist', async () => {
       const idNotFound = new OrderId('notFound')
-      const result = repository.get(idNotFound)
+      const result = await repository.get(idNotFound)
       expect(result.isFailure).toBeTruthy()
       expect(result.error).toEqual('Entity \'notFound\' not found')
     })
@@ -79,13 +81,13 @@ describe('InMemoryRepository', () => {
   /* -------------------------------------------------------------------------- */
 
   describe('.exists', () => {
-    it('returns true if entity exists', () => {
-      expect(repository.exists(order1.id)).toBeTruthy()
+    it('returns true if entity exists', async () => {
+      expect(await repository.exists(order1.id)).toBeTruthy()
     })
 
-    it('returns false if entity does not exist', () => {
+    it('returns false if entity does not exist', async () => {
       const idNotFound = new OrderId('notFound')
-      expect(repository.exists(idNotFound)).toBeFalsy()
+      expect(await repository.exists(idNotFound)).toBeFalsy()
     })
   })
 
@@ -94,14 +96,14 @@ describe('InMemoryRepository', () => {
   /* -------------------------------------------------------------------------- */
 
   describe('.delete', () => {
-    it('deletes entity', () => {
-      repository.delete(order1.id)
-      expect(repository.exists(order1.id)).toBeFalsy()
+    it('deletes entity', async () => {
+      await repository.delete(order1.id)
+      expect(await repository.exists(order1.id)).toBeFalsy()
     })
 
-    it('returns error if entity does not exist', () => {
+    it('returns error if entity does not exist', async () => {
       const idNotFound = new OrderId('notFound')
-      const result = repository.delete(idNotFound)
+      const result = await repository.delete(idNotFound)
       expect(result.isFailure).toBeTruthy()
       expect(result.error).toEqual('Entity \'notFound\' not found')
     })
@@ -112,21 +114,21 @@ describe('InMemoryRepository', () => {
   /* -------------------------------------------------------------------------- */
 
   describe('.find', () => {
-    it('returns entities if found', () => {
-      const result = repository.find(clientName('John'))
-      expect(result).toEqual([order1])
+    it('returns entities if found', async () => {
+      const result = await repository.find(clientName('John'))
+      expect(result.value).toEqual([order1])
     })
 
-    it('returns empty array if nothing found', () => {
-      const result = repository.find(clientName('not-found'))
-      expect(result).toEqual([])
+    it('returns empty array if nothing found', async () => {
+      const result = await repository.find(clientName('not-found'))
+      expect(result.value).toEqual([])
     })
 
-    it('raises exception if wrong logical operator passed', () => {
+    it('raises exception if wrong logical operator passed', async () => {
       const query = new Expression<Order>(
         'wrong' as LogicalOperators, clientName('John')
       )
-      expect(() => repository.find(query)).toThrowError('Invalid operator \'wrong\'')
+      await expect(() => repository.find(query)).rejects.toThrowError('Invalid operator \'wrong\'')
     })
   })
 })
