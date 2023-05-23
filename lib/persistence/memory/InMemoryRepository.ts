a2fdf0be3c0ead97176e3068e734662f7da51cf0
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2023-05-23 08:49:45 +0200

    feat: pagination (#68)

--- lib/persistence/memory/InMemoryRepository.ts
+++ lib/persistence/memory/InMemoryRepository.ts
@@ -1,41 +1,49 @@
 import { Aggregate, AnyIdentity } from '@lib/domain/models'
 import { Query } from '../Query'
-import { Repository } from '../Repository'
+import { Repository, ResultSet } from '../Repository'
 import { InMemoryQueryProcessor } from './InMemoryQueryProcessor'
 
 
 export class InMemoryRepository<
-  TEntity extends Aggregate<AnyIdentity>
-> implements Repository<TEntity> {
-  protected entities = new Map<TEntity['id'], TEntity>()
-  protected processor = new InMemoryQueryProcessor<TEntity>()
+  TAggregate extends Aggregate<AnyIdentity>
+> implements Repository<TAggregate> {
+  protected entities = new Map<TAggregate['id'], TAggregate>()
+  protected processor = new InMemoryQueryProcessor<TAggregate>()
 
-  async all(): Promise<readonly TEntity[]> {
-    return Array.from(this.entities.values())
+  async all(
+  // options?: QueryOptions,
+  ): Promise<ResultSet<TAggregate>> {
+    return new ResultSet(
+      Array.from(this.entities.values())
+    )
   }
 
-  async save(entity: TEntity): Promise<void> {
+  async save(entity: TAggregate): Promise<void> {
     const copy = Object.create(entity)
     Object.assign(copy, entity)
     this.entities.set(entity.id.value, copy)
   }
 
-  async get(id: TEntity['id']): Promise<TEntity> {
+  async get(id: TAggregate['id']): Promise<TAggregate> {
     const value = this.entities.get(id.value)
     if (!value) { throw new Error(`Entity '${id.value}' not found`) }
     return value
   }
 
-  async exists(id: TEntity['id']): Promise<boolean> {
+  async exists(id: TAggregate['id']): Promise<boolean> {
     return this.entities.has(id.value)
   }
 
-  async find(query: Query<TEntity>): Promise<readonly TEntity[]> {
+  async find(
+    query: Query<TAggregate>,
+    // options?: QueryOptions,
+  ): Promise<ResultSet<TAggregate>> {
     const entities = Array.from(this.entities.values())
-    return this.processor.execute(query, entities)
+    const result = this.processor.execute(query, entities)
+    return new ResultSet<TAggregate>(result) // no bookmark needed, we have all entities in memory
   }
 
-  async delete(id: TEntity['id']): Promise<void> {
+  async delete(id: TAggregate['id']): Promise<void> {
     const isExists = await this.exists(id)
     if (!isExists) { throw new Error(`Entity '${id.value}' not found`) }
     this.entities.delete(id.value)
